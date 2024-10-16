b3af9c4fdf5d35a922df0b1630eb42ae0577e757
Glados <ezrag2000@proton.me>
2024-10-15 18:11:54 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,60 +1,60 @@
-import json
 import os
 import shutil
 import datetime
+import tzlocal as tz
 
 
 class Main:
     def __init__(self, args):
+        global vars
         self.args = args
         self.save = None
         self.prev_dir = os.getcwd()
-        os.chdir(os.path.dirname(os.path.realpath(__file__)))
-        if not os.path.isfile('vars.json'):
-            with open('vars.json', 'w') as f:
-                f.write('{}')
-        self.vars = json.loads(open('vars.json', 'r').read())
+        self.vars = vars
+        self._current_dir = None
+        self._move_dir = None
+        self.tz = tz.get_localzone()
         os.chdir(self.prev_dir)
         self.parse_cmd()
 
     def parse_cmd(self):
-        if self.args[0] == 'help':
-            self.help()
-        elif self.args[0] == 'echo':
-            self.echo()
-        elif self.args[0] == 'mkvar':
-            self.mkvar()
-        elif self.args[0] == 'chvar':
-            self.chvar()
-        elif self.args[0] == 'delvar':
-            self.delvar()
-        elif self.args[0] == 'cd':
-            self.cd()
-        elif self.args[0] == 'ls':
-            self.ls()
-        elif self.args[0] == 'read':
-            self.read()
-        elif self.args[0] == 'rmv':
-            self.rmv()
-        elif self.args[0] == 'move':
-            self.move()
-        elif self.args[0] == 'clear':
-            self.clear()
-        elif self.args[0] == 'make':
-            self.make()
-        elif self.args[0] == 'time':
-            self.time()
-        elif self.args[0] == 'copy':
-            self.copy()
-        elif self.args[0] == 'pwd':
-            self.pwd()
-        else:
-            print('Error: Command not found!')
+        match self.args[0]:
+            case "help":
+                self.help()
+            case "echo":
+                self.echo()
+            case "mkvar":
+                self.mkvar()
+            case "chvar":
+                self.chvar()
+            case "delvar":
+                self.delvar()
+            case "cd":
+                self.cd()
+            case "ls":
+                self.ls()
+            case "read":
+                self.read()
+            case "rmv":
+                self.rmv()
+            case "move":
+                self.move()
+            case "clear":
+                self.clear()
+            case "make":
+                self.make()
+            case "time":
+                self.time()
+            case "copy":
+                self.copy()
+            case "pwd":
+                self.pwd()
+            case _:
+                print("Command not found!")
 
     def help(self):
         print("""
 echo -- echo <text>
-echo -var -- echo -var <var name>
 mkvar -- mkvar <var name> <value>
 chvar -- chvar <var name> <new value>
 delvar -- delvar <var name>
@@ -70,59 +70,83 @@ make -f -- make -f <file name>
 make -d -- make -d <directory name>
 time -- time
 time -format -- time -format <time format>
+INFO:
+Use % on most of the commands to use a variable (ex: echo %variable)
 """)
 
     def echo(self):
-        if self.args[1] == '-var':
-            print(self.vars.get(self.args[2]))
+        if self.args[1].startswith("%"):
+            print(self.vars.get(self.args[1][1:]))
         elif isinstance(self.args[1], str):
             print(self.args[1])
         else:
             print('Error: Not a string or a variable.')
 
     def mkvar(self):
-        os.chdir(os.path.dirname(os.path.realpath(__file__)))
         self.vars.update({self.args[1]: self.args[2]})
-        open('vars.json', 'w').write(json.dumps(self.vars))
 
     def chvar(self):
-        os.chdir(os.path.dirname(os.path.realpath(__file__)))
         self.vars[self.args[1]] = self.args[2]
-        open('vars.json', 'w').write(json.dumps(self.vars))
 
     def delvar(self):
-        os.chdir(os.path.dirname(os.path.realpath(__file__)))
         self.vars.pop(self.args[1])
-        open('vars.json', 'w').write(json.dumps(self.vars))
 
     def cd(self):
-        os.chdir(self.prev_dir)
-        os.chdir(self.args[1])
-        self.prev_dir = os.getcwd()
+        if self.args[1].startswith("%"):
+            os.chdir(self.vars.get(self.args[1][1:]))
+        else:
+            os.chdir(self.prev_dir)
+            os.chdir(self.args[1])
+            self.prev_dir = os.getcwd()
 
     def ls(self):
         if len(self.args[1:]) > 0:
             if self.args[1] == '-d':
-                self.prev_dir = os.getcwd()
-                os.chdir(self.args[2])
-                for i in os.listdir():
-                    print(i)
-                os.chdir(self.prev_dir)
+                if self.args[2].startswith("%"):
+                    self.prev_dir = os.getcwd()
+                    os.chdir(self.vars.get(self.args[2][1:]))
+                    for i in os.listdir():
+                        print(i)
+                    os.chdir(self.prev_dir)
+                else:
+                    self.prev_dir = os.getcwd()
+                    os.chdir(self.args[2])
+                    for i in os.listdir():
+                        print(i)
+                    os.chdir(self.prev_dir)
         else:
             for i in os.listdir():
                 print(i)
 
     def read(self):
-        print(open(self.args[1], 'r').read())
+        if self.args[1].startswith("%"):
+            print(open(self.vars.get(self.args[1][1:]), 'r').read())
+        else:
+            print(open(self.args[1], 'r').read())
 
     def rmv(self):
         if self.args[1] == '-f':
-            os.remove(self.args[2])
+            if self.args[2].startswith("%"):
+                os.remove(self.vars.get(self.args[2][1:]))
+            else:
+                os.remove(self.args[2])
         elif self.args[1] == '-d':
-            os.rmdir(self.args[2])
+            if self.args[2].startswith("%"):
+                os.rmdir(self.vars.get(self.args[2][1:]))
+            else:
+                os.rmdir(self.args[2])
 
     def move(self):
-        shutil.move(self.args[1], self.args[2])
+        if self.startswith(self.args[1], "%"):
+            self._current_dir = self.vars.get(self.args[1][1:])
+        else:
+            self._current_dir = self.args[1]
+
+        if self.startswith(self.args[2], "%"):
+            self._move_dir = self.vars.get(self.args[2][1:])
+        else:
+            self._move_dir = self.args[2]
+        shutil.move(self._current_dir, self._move_dir)
 
     def clear(self):
         if os.name == 'nt':
@@ -132,35 +156,64 @@ time -format -- time -format <time format>
 
     def make(self):
         if self.args[1] == '-d':
-            os.mkdir(self.args[2])
+            if self.startswith(self.args[2], "%"):
+                os.mkdir(self.vars.get(self.args[2][1:]))
+            else:
+                os.mkdir(self.args[2])
         if self.args[1] == '-f':
-            with open(self.args[2], 'w') as file:
-                file.write('')
-                file.close()
+            if self.startswith(self.args[2], "%"):
+                with open(self.vars.get(self.args[2][1:]), 'w') as file:
+                    file.write('')
+                    file.close()
+            else:
+                with open(self.args[2], 'w') as file:
+                    file.write('')
+                    file.close()
 
     def copy(self):
-        shutil.copy(self.args[1], self.args[2])
+        if self.startswith(self.args[1], "%"):
+            self._current_dir = self.vars.get(self.args[1][1:])
+        else:
+            self._current_dir = self.args[1]
+
+        if self.startswith(self.args[2], "%"):
+            self._move_dir = self.vars.get(self.args[2][1:])
+        else:
+            self._move_dir = self.args[2]
+        shutil.copy(self._current_dir, self._move_dir)
 
     def time(self):
         if len(self.args) > 1:
             if self.args[1] == '-format':
-                print(datetime.datetime.now().strftime(self.args[2]))
+                if self.startswith(self.args[2], "%"):
+                    print(datetime.datetime.now().strftime(self.vars.get(self.args[2][1:])))
+                else:
+                    print(datetime.datetime.now(self.tz).strftime(self.args[2]))
         else:
-            print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            print(datetime.datetime.now(self.tz).strftime('%Y-%m-%d %H:%M:%S'))
 
     def pwd(self):
         print(os.getcwd())
 
+    def return_vars(self):
+        return self.vars
+
+    def startswith(self, _string: str, _char: str):
+        if _string.startswith(_char):
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
     args = []
+    vars = {}
     while True:
         inp = input(f'{os.getcwd()} % ')
         args.clear()
         for i in inp.split(' '):
             args.append(i)
         try:
-            Main(args)
+            vars.update(Main(args).return_vars())
         except:
             print('Error: Failed to execute command!')
-
